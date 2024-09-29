d964b5c47ef48800ae7e2edcc83c14a0073bfeff
Iasmim Horrana <horranaiasmim@gmail.com>
2024-09-29 15:56:17 -0300

    update src

---
+++ src/load_data.py
@@ -0,0 +1,16 @@
+import pandas as pd
+from sqlalchemy import create_engine
+
+# Conexão com o banco de dados PostgreSQL no Docker
+engine = create_engine('postgresql+psycopg2://postgres:Postgres2024!@localhost:15432/postgres')
+
+# Caminho para o CSV dentro da pasta data
+csv_path = '../data/Big_Japan_vs_China_Technology.csv'
+
+# Ler o arquivo CSV
+df = pd.read_csv(csv_path)
+
+# Inserir o DataFrame no banco de dados
+df.to_sql('dataset_table', engine, if_exists='replace', index=False)
+
+print("Dados inseridos com sucesso!")
