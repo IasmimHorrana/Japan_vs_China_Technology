0f7d3f322ead30dea89f3dd6d9d1618d7a27e3ac
Iasmim Horrana <horranaiasmim@gmail.com>
2024-10-07 21:05:58 -0300

    update-load-clean-data

--- src/load_data.py
+++ src/load_data.py
@@ -1,5 +1,6 @@
 import pandas as pd
 from sqlalchemy import create_engine
+from transform_data import clean_data  # Importa o arquivo de transformação
 
 # Conexão com o banco de dados PostgreSQL no Docker
 engine = create_engine('postgresql+psycopg2://postgres:Postgres2024!@localhost:15432/postgres')
@@ -10,8 +11,11 @@ csv_path = '../data/Big_Japan_vs_China_Technology.csv'
 # Ler o arquivo CSV
 df = pd.read_csv(csv_path)
 
+# Limpar e transformar os dados
+df = clean_data(df)
+
 # Inserir o DataFrame no banco de dados 
-df.to_sql('dataset_table', engine, if_exists='append', index=False) #if_exists='replace'
+df.to_sql('dataset_table', engine, if_exists='replace', index=False) #if_exists='append'
 
 print("Dados inseridos com sucesso!")
 
